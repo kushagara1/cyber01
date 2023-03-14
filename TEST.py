aa952785cb69bcbff48b400c6e6a82e612f20276
Kushagra Saxena <77769259+kushagara1@users.noreply.github.com>
2023-03-15 03:14:01 +0530

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,5 @@
+from cmath import sqrt
+import math
+a=49
+w= math.sqrt(a)
+print=(w)
---
+++ project-1.py
@@ -0,0 +1,23 @@
+import zipfile
+
+def extractFile(zfile, password):
+    try:
+        zfile.extractall(pwd=bytes(password, 'utf-8'))
+        return password
+    except:
+        return
+    
+
+def main():
+    zfile=zipfile.ZipFile('test.zip')
+    passFile=open('passlist.txt')
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        guess = extractFile(zipfile, password)
+        if guess:
+            print('Password = ' +password)
+            break
+
+
+if __name__== '__main__':
+    main()
